623fcfadc85c1f92a543adbb6e3482f7f5599675
ykaya932 <ykaya932@gmail.com>
2020-05-17 00:25:41 +0300

    Added validation of parameters.

--- practice_app/shipment_calculator/urls.py
+++ practice_app/shipment_calculator/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path(r'', views.calculate_price, name="calculate_price"),
-    # path("calculate_shipment_price?<latitude>&<longitude>&<int:product_id>", calculate_price, name="calculate_price"),
 ]
--- practice_app/shipment_calculator/views.py
+++ practice_app/shipment_calculator/views.py
@@ -3,14 +3,27 @@ from django.http import JsonResponse, HttpResponse
 
 # def calculate_price(request, latitude, longitude, product_id):
 def calculate_price(request):
-
     latitude = request.GET.get('lat', None)
     longitude = request.GET.get('lon', None)
     product_id = request.GET.get('pid', None)
 
     if (not latitude) or (not longitude) or (not product_id):
-        return HttpResponse("Missing Information. Please try to use ...")
+        return HttpResponse("Missing Information. Please try to use the following format: "
+                            "url_here/?lat=\"Latitude\"&lon=\"Longitude\"&pid=\"Product_ID\"/")
 
-    # TODO check the coordinates and product id
+    try:
+        latitude = float(latitude)
+        longitude = float(latitude)
+    except ValueError:
+        return HttpResponse("Make sure that latitude and longitude are given as float number. Ex: lat=14.23&lon=41.12")
+
+    if not (-90 <= latitude <= 90):
+        return HttpResponse("Given Latitude is not in range of [-90, 90]")
+
+    if not (-180 <= longitude <= 180):
+        return HttpResponse("Given Longitude is not in range of [-180, 180]")
+
+    # if can not find product:
+    #     return HttpResponse("Product_ID is invalid.")
 
     return HttpResponse(f"Your latitude: {latitude}, Your Longitude: {longitude}")
