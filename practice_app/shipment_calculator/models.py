9cdb4d8fd7a3400665aa9a3f5914e1574cbe9340
ykaya932 <ykaya932@gmail.com>
2020-05-20 17:14:05 +0300

    Created the Customer Model

--- practice_app/shipment_calculator/models.py
+++ practice_app/shipment_calculator/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+CARGO_COMPANY_CHOICES = (
+    ('aras', 'Aras Kargo'),
+    ('mng', 'MNG Kargo'),
+    ('yurtici', 'Yurtici Kargo'),
+    ('ptt', 'PTT Kargo'),
+    ('ups', 'UPS'),
+    ('other', 'Other')
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    amount_left = models.PositiveIntegerField()
+    price = models.FloatField(
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    brand = models.CharField(max_length=200)
+    is_free_shipment = models.BooleanField()
+    release_date = models.DateTimeField(default=timezone.now)
+    cargo_company = models.CharField(max_length=10,
+                                     choices=CARGO_COMPANY_CHOICES,
+                                     default='other')
+
+    lat = models.FloatField(
+        validators=[
+            MinValueValidator(-90),
+            MaxValueValidator(90)
+        ]
+    )
+    lon = models.FloatField(
+        validators=[
+            MinValueValidator(-180),
+            MaxValueValidator(180)
+        ]
+    )
+
+    def release_product(self):
+        self.release_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
