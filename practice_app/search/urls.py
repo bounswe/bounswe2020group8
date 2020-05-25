f25947520bec59720dce945411bfd871eaed7b15
Afra Akbaş <akbasafra.aa@gmail.com>
2020-05-26 00:15:12 +0300

    Remove dublicates

--- practice_app/search/urls.py
+++ practice_app/search/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("search/", views.search, name="search"),
-    path("", views.search, name="home"),
-    path("search/<slug:slug>", views.search2, name="search2"),
+    path('', views.search, name="search"),
 ]
