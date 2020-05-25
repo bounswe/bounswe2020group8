0dff3f91003cd6cb1d98a327517555496db55fa9
Afra Akbaş <akbasafra.aa@gmail.com>
2020-05-25 15:08:55 +0300

    Added tests.py

--- practice_app/search/urls.py
+++ practice_app/search/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("search/", views.search, name="search"),
     path("", views.search, name="home"),
+    path("search/<slug:slug>", views.search2, name="search2"),
 ]
--- practice_app/search/views.py
+++ practice_app/search/views.py
@@ -28,3 +28,27 @@ def search(request):
     n = productList["Product"]
     return render(request, 'search/search.html', {"name": n, "key": "Search was found", "word": queryKeywords})
 
+def search2(request, slug):
+    key = API_key
+    queryKeywords = ''
+    if request.method == 'POST':
+        if request.POST.get("search"):
+            queryKeywords = request.POST.get("product")
+    else:
+        queryKeywords = slug
+    response = requests.get('https://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=%s&siteid=0&version=967&QueryKeywords=%s&MaxEntries=25' 
+                                %(key, queryKeywords))
+    
+    productList = response.json()
+    if productList['Ack'] == 'Failure':
+        errorMessages = productList['Errors'][0]
+        if errorMessages['ShortMessage'] == 'Web Service framework internal error.':
+            return render(request, 'search/search.html', {"name": productList, "key": "Please enter a keyword for search"})
+        elif errorMessages['ShortMessage'] == 'No match found.':
+            return render(request, 'search/search.html', {"name": productList, "key": "No match found"})
+        else:
+            return render(request, 'search/search.html', {"name": productList, "key": "Something goes wrong, please try again!"})
+    n = productList["Product"]
+    return render(request, 'search/search.html', {"name": n, "key": "Search was found", "word": queryKeywords})
+
+
