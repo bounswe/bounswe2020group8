4dc58e4edebf830a730265bf445c686eef164c85
Burak Berk Özer <burakberkozer@gmail.com>
2020-05-26 18:28:21 +0300

    Adding covid_tracker api with styled results_page

---
+++ practice_app/covid_tracker/tracker/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from tracker.tracker_api import get_country
+# Create your views here.
+
+# def index(request):
+#     return HttpResponse('Hello!')
+
+def index(request):
+    my_dict = {'insert_me': "Hello I am from views.py!"}
+    return render(request, 'tracker/index.html', context=my_dict)
+
+def track(request):
+    my_dict = {'insert_me': 'Table will be here.'}
+    return render(request, 'tracker/tracking_page.html', context=my_dict)
+
+def track_results(request):
+
+    if request.method == 'POST':
+        country = request.POST.get('textfield', None)
+        country = country.lower().capitalize()
+        # try:
+            # Get country data
+        country_results = get_country(country)
+        my_dict = {'country': country, 'country_results': country_results}
+        print()
+        return render(request, 'tracker/result_page.html', context=my_dict)
+        # except:
+        #     return HttpResponse("Invalid Country!")
+    else:
+        return render(request, 'tracker/tracking_page.html')
