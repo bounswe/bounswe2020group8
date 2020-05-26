130e557e509734a52071b7a5a2bb4b6e6c1d2ad4
ykaya932 <ykaya932@gmail.com>
2020-05-26 19:09:12 +0300

    Added Product Form Tests

--- practice_app/shipment_calculator/tests/test_forms.py
+++ practice_app/shipment_calculator/tests/test_forms.py
@@ -63,3 +63,83 @@ class VendorFormTest(TestCase):
 
         self.assertFalse(form.is_valid())
 
+class ProductFormTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Vendor.objects.create(
+            first_name='Yasin',
+            last_name='Kaya',
+            email='ykaya932@gmail.com',
+            lat=30,
+            lon=40,
+        )
+
+    def test_product_form_with_valid_data(self):
+        nice_vendor = Vendor.objects.get(email='ykaya932@gmail.com')
+        form = ProductForm(data={
+            'vendor': nice_vendor,
+            'title': 'Apple Watch v32',
+            'description': 'An awesome watch.',
+            'amount_left': 100,
+            'price': 2000,
+            'brand': 'Apple',
+            'release_date': timezone.now(),
+            'cargo_company': 'aras'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_product_form_without_vendor(self):
+        nice_vendor = Vendor.objects.get(email='ykaya932@gmail.com')
+        form = ProductForm(data={
+            'vendor': None,
+            'title': 'Apple Watch v32',
+            'description': 'An awesome watch.',
+            'amount_left': 100,
+            'price': 2000,
+            'brand': 'Apple',
+            'release_date': timezone.now(),
+            'cargo_company': 'aras'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_product_form_invalid_cargo_company(self):
+        nice_vendor = Vendor.objects.get(email='ykaya932@gmail.com')
+        form = ProductForm(data={
+            'vendor': nice_vendor,
+            'title': 'Apple Watch v32',
+            'description': 'An awesome watch.',
+            'amount_left': 100,
+            'price': 2000,
+            'brand': 'Apple',
+            'release_date': timezone.now(),
+            'cargo_company': 'fast_cargo_company'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_product_form_float_amount_left(self):
+        nice_vendor = Vendor.objects.get(email='ykaya932@gmail.com')
+        form = ProductForm(data={
+            'vendor': nice_vendor,
+            'title': 'Apple Watch v32',
+            'description': 'An awesome watch.',
+            'amount_left': 0.5,
+            'price': 2000,
+            'brand': 'Apple',
+            'release_date': timezone.now(),
+            'cargo_company': 'aras'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_product_form_negative_price(self):
+        nice_vendor = Vendor.objects.get(email='ykaya932@gmail.com')
+        form = ProductForm(data={
+            'vendor': nice_vendor,
+            'title': 'Apple Watch v32',
+            'description': 'An awesome watch.',
+            'amount_left': 1,
+            'price': -20,
+            'brand': 'Apple',
+            'release_date': timezone.now(),
+            'cargo_company': 'aras'
+        })
+        self.assertFalse(form.is_valid())
