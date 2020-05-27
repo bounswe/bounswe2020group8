22fea68325941d9a5da357286582f83e0cc72c40
Nursima <nursima.celik@boun.edu.tr>
2020-05-27 23:41:57 +0300

    Changed settings.py

--- practice_app/practice_app/settings.py
+++ practice_app/practice_app/settings.py
@@ -37,11 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'recommend.apps.RecommendConfig',
-=======
     'shipment_calculator',
->>>>>>> origin/master
     'search.apps.SearchConfig',
 ]
 
