025b4326e9e6a1e77fa9e0d804f6a865f379c10e
Nursima <nursima.celik@boun.edu.tr>
2020-05-27 23:51:58 +0300

    Delete urls.py recommendation

--- practice_app/recommend/urls.py
+++ practice_app/recommend/urls.py
@@ -2,19 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recommendation/album/', views.recommendation_album, name='recommendation_album'),
-    path('recommendation/customer/<int:pk>/', views.recommendation, name='recommendation'),
+    path('album/', views.recommendation_album, name='recommendation_album'),
+    path('customer/<int:pk>/', views.recommendation, name='recommendation'),
 
-    path('recommendation/customers/', views.customer_list, name='customer_list'),
-    path('recommendation/customer/detail/<int:pk>/', views.customer_detail, name='customer_detail'),
-    path('recommendation/customer/new/', views.customer_new, name='customer_new'),
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customer/detail/<int:pk>/', views.customer_detail, name='customer_detail'),
+    path('customer/new/', views.customer_new, name='customer_new'),
 
 
-    path('recommendation/products/', views.product_list, name='product_list'),
-    path('recommendation/product/<int:pk>/', views.product_detail, name='product_detail'),
-    path('recommendation/product/new/', views.product_new, name='product_new'),
-    path('recommendation/product/<int:pk>/edit/', views.product_edit, name='product_edit'),
-    path('recommendation/orderedproducts/<int:pk>', views.ordered_product_list, name='ordered_product_list'),
+    path('products/', views.product_list, name='product_list'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/new/', views.product_new, name='product_new'),
+    path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),
+    path('orderedproducts/<int:pk>', views.ordered_product_list, name='ordered_product_list'),
     
 ]
 
