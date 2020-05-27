61b407c001d97f5e6c1741f7f3dbbd198b8f5baa
ykaya932 <44112869+ykaya932@users.noreply.github.com>
2020-05-27 13:27:33 +0300

    Revert "Adding covid_tracker api with styled results_page"

--- practice_app/covid_tracker/tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from tracker import views
-
-urlpatterns = [
-    path('', views.track, name='Track Page'),
-    path('results/', views.track_results, name='Result Page'),
-]
