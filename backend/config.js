ed6a50e0239d4732e5fdf1b8411c1600a9121734
kaandura <kaan.dura@tavan.app>
2020-11-12 18:30:50 +0300

    inital backend

--- backend/config.js
+++ backend/config.js
@@ -6,8 +6,8 @@ const config = {
     db: process.env.MAIN_DB,
     s3: {
       accessKeyId: process.env.S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+    }
   },
   dev: {
     port: process.env.PORT || 8080,
@@ -15,12 +15,10 @@ const config = {
       process.env.MAIN_DB ||
       "mongodb+srv://testRW:SC3oOJMv25kxb4dH@cluster0.4vqc5.mongodb.net/dev?retryWrites=true",
     s3: {
-      accessKeyId: process.env.S3_ACCESS_KEY_ID || "AKIAYKBH3PBY2I4WZNMW",
-      secretAccessKey:
-        process.env.S3_SECRET_ACCESS_KEY ||
-        "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",
-    },
-  },
+      accessKeyId: process.env.S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+    }
+  }
 };
 
 module.exports = config[process.env.NODE_ENV || Constants.NODE_ENV];
