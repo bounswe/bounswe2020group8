a9968ba8022deca89c2d8b585d04c2505925d960
Berke.metin <berke.metin@boun.edu.tr>
2020-11-22 18:21:41 +0300

    Account page edited.

--- Android/app/src/main/java/com/example/carousel/MemberAccountPageFragment.kt
+++ Android/app/src/main/java/com/example/carousel/MemberAccountPageFragment.kt
@@ -1,13 +1,15 @@
 package com.example.carousel
 
-import android.content.Intent
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.carousel.R.string
+import com.example.carousel.R.drawable
 import kotlinx.android.synthetic.main.fragment_acount_page.*
 import kotlinx.android.synthetic.main.fragment_acount_page.view.*
 
@@ -25,31 +27,31 @@ class MemberAccountPageFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         mAdapter =  CustomAdapter(context as Context)
         mAdapter.addSectionHeaderItem("Account" )
-        mAdapter.addItem("User Information", string.fa_user_solid )
-        mAdapter.addItem("Change Password", string.fa_key_solid)
-        mAdapter.addItem("Settings",string.fa_cog_solid )
-        mAdapter.addItem("Logout",string.fa_sign_out_alt_solid )
+        mAdapter.addItem("User Information", drawable.ic_person )
+        mAdapter.addItem("Change Password", drawable.ic_key)
+        mAdapter.addItem("Settings",drawable.ic_settings )
+        mAdapter.addItem("Logout",drawable.ic_exit )
         mAdapter.addSectionHeaderItem("Carousel" )
-        mAdapter.addItem("About",string.fa_info_solid )
-        mAdapter.addItem("Legals",string.fa_file_contract_solid)
-        mAdapter.addItem("Contact", string.fa_id_card )
+        mAdapter.addItem("About", drawable.ic_info)
+        mAdapter.addItem("Legals", drawable.ic_file)
+        mAdapter.addItem("Contact", drawable.ic_contact)
         listView.adapter = (mAdapter)
-        if (login == 0){
+        if (login == 1){
             view.guest.visibility = View.VISIBLE
             val intent = Intent(activity, LoginActivity::class.java)
             startActivityForResult(intent,11)
         }else{
-            view.guest.visibility = View.VISIBLE
+            view.login_user.visibility = View.VISIBLE
         }
         view.login_button.setOnClickListener {
             val intent = Intent(activity, LoginActivity::class.java)
             startActivityForResult(intent,11)
         }
+        listView.onItemClickListener = OnItemClickListener { adapterView, view, pos, l ->
+            Toast.makeText(context, pos.toString(),Toast.LENGTH_SHORT).show()
+            //TicketList Object
+        }
 	}
-
-    override fun onResume() {
-        super.onResume()
-    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
