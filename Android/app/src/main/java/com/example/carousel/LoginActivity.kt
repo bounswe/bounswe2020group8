16182cfc0d3dcf7d9ff784e8c6ccf9b3fe6dbb89
OnurEnginer <onurenginer1@gmail.com>
2020-11-23 15:11:28 +0300

    Backend connection for google login and some UI changes

--- Android/app/src/main/java/com/example/carousel/LoginActivity.kt
+++ Android/app/src/main/java/com/example/carousel/LoginActivity.kt
@@ -10,8 +10,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.synthetic.main.activity_login.*
@@ -50,10 +52,9 @@ class LoginActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val account = GoogleSignIn.getLastSignedInAccount(this)
-        if(account!=null) {
-            val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
-            startActivity(intent)
-        }
+        //if(account!=null) {
+
+        //}
     }
     fun login(view: View){
         val email = findViewById<EditText>(R.id.login_email).text.toString()
@@ -84,13 +85,50 @@ class LoginActivity : AppCompatActivity() {
 
 
             override fun onResponse(call: Call, response: Response) {
-                val json = Gson().fromJson(response.body?.string(), HttpResponse::class.java)
+                val json = Gson().fromJson(response.body?.string(), LoginWithPasswordJSON::class.java)
                 val responseCode = response.code
                 if(responseCode == 200) {
                     this@LoginActivity.runOnUiThread(Runnable { //Handle UI here
                         val intent = Intent()
                         intent.putExtra("token", json.tokenCode)
                         intent.putExtra("login", 1)
+                        intent.putExtra("fullName", "${json.client.name} ${json.client.lastName}")
+                        setResult(RESULT_OK, intent)
+                        finish();
+                    })
+                }
+                else{
+                    this@LoginActivity.runOnUiThread( Runnable {
+                        Toast.makeText(this@LoginActivity, "Invalid email or password", Toast.LENGTH_SHORT).show()
+                    })
+                }
+            }
+        })
+    }
+    private fun signUpCall(email: String?, googleId: String?){
+        val postBody = FormBody.Builder().build()
+        val httpUrl = "$baseUrl/client/loginWithGoogle?email=$email&googleId=$googleId"
+        val request = Request.Builder()
+            .addHeader("accept", "application/json")
+            .url(httpUrl)
+            .post(postBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Failure", e.stackTraceToString())
+            }
+
+
+            override fun onResponse(call: Call, response: Response) {
+                val json = Gson().fromJson(response.body?.string(), SignInWithPasswordJSON::class.java)
+                val responseCode = response.code
+                if(responseCode == 200) {
+                    this@LoginActivity.runOnUiThread(Runnable { //Handle UI here
+                        val intent = Intent()
+                        intent.putExtra("token", json.tokenCode)
+                        intent.putExtra("login", 1)
+                        intent.putExtra("fullName", "${json.client.name} ${json.client.lastName}")
                         setResult(RESULT_OK, intent)
                         finish();
                     })
@@ -110,16 +148,41 @@ class LoginActivity : AppCompatActivity() {
     fun signIn() {
         val intent = mGoogleSignInClient!!.signInIntent
         startActivityForResult(intent, RC_SIGN_IN)
+
+    }
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account :GoogleSignInAccount? = task.getResult(ApiException::class.java)
+                signUpCall(googleId = account!!.id, email = account.email)
+            } catch (e: ApiException) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.e("TAG","signInResult:failed code=" + e.statusCode)
+            }
+        }
     }
 }
 
-data class HttpResponse (
+data class LoginWithPasswordJSON (
     @SerializedName("tokenCode") val tokenCode : String,
     @SerializedName("returnCode") val returnCode: Int,
     @SerializedName("returnMessage") val returnMessage: String,
-    @SerializedName("client") val client: Client
+    @SerializedName("client") val client: ClientJSON
 )
-data class Client (
+data class SignInWithPasswordJSON (
+    @SerializedName("tokenCode") val tokenCode : String,
+    @SerializedName("client") val client: ClientJSON
+)
+data class ClientJSON (
     @SerializedName("id") val id : String,
     @SerializedName("name") val name : String,
     @SerializedName("lastName") val lastName : String,
