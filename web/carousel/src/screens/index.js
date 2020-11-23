f35978a800c15bf26d0d7b95670bb74b3cb6b348
Öncel Keleş <31131763+oncelkeles@users.noreply.github.com>
2020-11-23 18:00:56 +0300

    Update screens/index.js to use context

--- web/carousel/src/screens/index.js
+++ web/carousel/src/screens/index.js
@@ -1,20 +1,34 @@
 import React from "react";
+import {useContext, useState} from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
+import UserInfo from "../components/Context/UserInfo";
 
 import Header from "../components/Header/Header";
 
 const App = () => {
+  const [email, setEmail] = useState("");
+
+  const loginHandler = (newEmail) => {
+      setEmail(newEmail);
+  };
+  
+  console.log("email: " + email);
   return (
     <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" exact component={Login} />
-        </Switch>
-      </Router>
+      <UserInfo.Provider value={{
+                email: email,
+                login: loginHandler
+            }}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+          </Switch>
+        </Router>
+      </UserInfo.Provider>
     </>
   );
 };
