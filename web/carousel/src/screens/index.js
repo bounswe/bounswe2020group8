a3626085fa1e5c0285bbb7e53f2e76a734d471f0
Öncel Keleş <31131763+oncelkeles@users.noreply.github.com>
2020-11-27 17:35:44 +0300

    New context handlers and values are added

--- web/carousel/src/screens/index.js
+++ web/carousel/src/screens/index.js
@@ -3,7 +3,7 @@ import { useContext, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
-import SignUp from "./SignUp";
+import Reset from "./Reset";
 import Forgot from "./Forgot";
 import UserInfo from "../components/Context/UserInfo";
 
@@ -12,17 +12,58 @@ import Header from "../components/Header/Header";
 const App = () => {
   const [email, setEmail] = useState("");
   const [token, setToken] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [userType, setUserType] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [error, setError] = useState(false);
 
   const loginHandler = (newEmail, newToken) => {
     setEmail(newEmail);
     setToken(newToken);
   };
+  const emailChangeHandler = (newEmail) => {
+      setEmail(newEmail);
+  }
+  const passwordChangeHandler = (newPassword) => {
+      setPassword(newPassword);
+  }
+  const confirmPasswordChangeHandler = (newConfirmPassword) => {
+      setPasswordConfirm(newConfirmPassword);
+  }
+  const userTypeChangeHandler = (newUserType) => {
+      setUserType(newUserType);
+  }
+  const companyNameChangeHandler = (newCompanyName) => {
+      setCompanyName(newCompanyName);
+  }
+  const setErrorHandler = (newError) => {
+      setError(newError);
+  }
+  
   return (
-    <>
+    <div>
       <UserInfo.Provider
         value={{
           email: email,
+          password: password,
+          passwordConfirm: passwordConfirm,
+          userType: userType,
+          name: name,
+          surname: surname,
+          companyName: name,
+          error: error,
           login: loginHandler,
+          changeEmail:emailChangeHandler,
+          setPassword: passwordChangeHandler,
+          setUserType: userTypeChangeHandler,
+          setCompanyName: companyNameChangeHandler,
+          setName: setName,
+          setSurname: setSurname,
+          setPasswordConfirm: confirmPasswordChangeHandler,
+          setError: setErrorHandler
         }}
       >
         <Router>
@@ -30,12 +71,12 @@ const App = () => {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/login" exact component={Login} />
-            <Route path="/signup" exact component={SignUp} />
+            <Route path="/reset" exact component={Reset} />
             <Route path="/forgot" exact component={Forgot} />
           </Switch>
         </Router>
       </UserInfo.Provider>
-    </>
+    </div>
   );
 };
 
