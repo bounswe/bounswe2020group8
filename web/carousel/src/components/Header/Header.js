2e6e4335ca493a596b02a438c7303cb14c3f942a
Öncel Keleş <31131763+oncelkeles@users.noreply.github.com>
2020-11-29 00:47:07 +0300

    indentation update

--- web/carousel/src/components/Header/Header.js
+++ web/carousel/src/components/Header/Header.js
@@ -11,73 +11,77 @@ import classes from './Header.module.css';
 import logo from '../../assets/images/carousel_logo.jpg';
 
 class Header extends Component {
-    state = {
-        searchValue: "",
-        placeholderSearchString: "Search for something...",
-        searchOn: false,
-    }
+  state = {
+    searchValue: "",
+    placeholderSearchString: "Search for something...",
+    searchOn: false,
+  }
 
-    categories = ["Fashion", "Toys & Hobbies", "Electronics", "Furniture", "Personal Care"];
+  categories = [
+    "Fashion",
+    "Toys & Hobbies",
+    "Electronics",
+    "Furniture",
+    "Personal Care",
+  ];
 
-    openProfileHandler = () => {
-        alert("Open Profile");
-    }
+  openProfileHandler = () => {
+    alert("Open Profile");
+  }
 
-    searchStringChangeHandler = (event, id) => {
-        this.setState({searchValue: ""});
-        let searchValue = event.target.value;
-        if (!this.state.searchOn) {
-            searchValue = searchValue.substr(searchValue.length - 1, 1);
-        }
-        this.setState({searchValue: searchValue, searchOn: true});
+  searchStringChangeHandler = (event, id) => {
+    this.setState({searchValue: ""});
+    let searchValue = event.target.value;
+    if (!this.state.searchOn) {
+      searchValue = searchValue.substr(searchValue.length - 1, 1);
     }
+    this.setState({searchValue: searchValue, searchOn: true});
+  }
 
-    // detect enter on search bar
-    keyPressHandler = (e) => {
-        if (e.key === "Enter") {
-            alert("Searching");
-        }
+  // detect enter on search bar
+  keyPressHandler = (e) => {
+    if (e.key === "Enter") {
+      alert("Searching");
     }
+  }
 
-    // clicked on search incon
-    iconPressHandler = (e) => {
-        alert("Searching");
-    }
+  // clicked on search incon
+  iconPressHandler = (e) => {
+    alert("Searching");
+  }
 
-    render() {
-        return (
-            <>
-            <div className={classes.Header}>
-                <div className={classes.Toolbar}>
-                    <img 
-                        src={logo} 
-                        alt={"carouselSite"} 
-                        className={classes.Img}
-                        onClick={() => this.props.history.push("/")}/>
-                    <SearchBar
-                        searchString={this.state.searchValue}
-                        changeString={this.searchStringChangeHandler}
-                        written={this.state.searchOn}
-                        defaultString={this.state.placeholderSearchString}
-                        keyHandler={this.keyPressHandler}
-                        iconHandler={this.iconPressHandler}/>
-                    <SideButtons
-                        authenticated={this.props.isSignedIn}
-                        clicked={this.props.isSignedIn ? this.openProfileHandler : () => this.props.history.push("/login") }/>
-                </div>
-                {/*<Categories categories={this.categories}/>*/}
-            </div>
-            <div className={classes.Filler} />
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className={classes.Header}>
+          <div className={classes.Toolbar}>
+            <img 
+              src={logo} 
+              alt={"carouselSite"} 
+              className={classes.Img}
+              onClick={() => this.props.history.push("/")}/>
+            <SearchBar
+              searchString={this.state.searchValue}
+              changeString={this.searchStringChangeHandler}
+              written={this.state.searchOn}
+              defaultString={this.state.placeholderSearchString}
+              keyHandler={this.keyPressHandler}
+              iconHandler={this.iconPressHandler}/>
+            <SideButtons />
+          </div>
+          {/*<Categories categories={this.categories}/>*/}
+        </div>
+      <div className={classes.Filler} />
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        isSignedIn: state.auth.isSignedIn,
-        googleId: state.auth.googleId
-    };
+  return {
+    isSignedIn: state.auth.isSignedIn,
+    googleId: state.auth.googleId
+  };
 }
 
 export default withRouter(connect(mapStateToProps, { signIn, signOut })(Header));
