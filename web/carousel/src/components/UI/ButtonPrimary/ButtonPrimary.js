773c4611d963df79f313a6eb31aea244ff7e4530
Öncel Keleş <31131763+oncelkeles@users.noreply.github.com>
2020-11-28 22:29:46 +0300

    Update ButtonPrimary.js

--- web/carousel/src/components/UI/ButtonPrimary/ButtonPrimary.js
+++ web/carousel/src/components/UI/ButtonPrimary/ButtonPrimary.js
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from "./ButtonPrimary.module.css";
 
-const ButtonPrimary = ({ icon, title }) => {
+const ButtonPrimary = ({ icon, title, style={}, onClick}) => {
   return (
-    <button className={classes.ButtonPrimary}>
+    <button className={classes.ButtonPrimary} style={style} onClick={onClick}>
       {icon || null}
       <div> {title}</div>
     </button>
