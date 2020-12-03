9dda34763118b86753d8ebcdf3145703145ddf99
Afra Akbaş <akbasafra.aa@gmail.com>
2020-12-03 16:54:14 +0300

    Logout from account

--- web/carousel/src/components/Header/SideButtons/SideButtons.js
+++ web/carousel/src/components/Header/SideButtons/SideButtons.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./SideButtons.module.css";
 import ButtonPrimary from "../../UI/ButtonPrimary/ButtonPrimary";
 import ButtonSecondary from "../../UI/ButtonSecondary/ButtonSecondary";
@@ -19,63 +19,69 @@ import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { signIn, signOut } from "../../../redux/auth/actions";
 
-const profileMenu = (
-  <Menu>
-    <Menu.Item>
-      <Link to="/account/profile">
-        <UserOutlined />
-        My Profile
-      </Link>
-    </Menu.Item>
-    <Menu.Item>
-      <Link to="/account/active-order">
-        <ShoppingOutlined />
-        My Order
-      </Link>
-    </Menu.Item>
-    <Menu.Item>
-      <Link to="/account/comments">
-        <CommentOutlined />
-        My Feedbacks
-      </Link>
-    </Menu.Item>
-    <Menu.Item>
-      <Link to="/account/recommendation">
-        <NotificationOutlined />
-        New Recommendations
-      </Link>
-    </Menu.Item>
-    <Menu.Item key="Logout">
-      <Link to="/">
-        <LogoutOutlined />
-        Log out
-      </Link>
-    </Menu.Item>
-  </Menu>
-);
+class SideButtons extends Component {
+  profileMenu() {
+    return (
+      <Menu>
+        <Menu.Item>
+          <Link to="/account/profile">
+            <UserOutlined />
+            My Profile
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/account/active-order">
+            <ShoppingOutlined />
+            My Order
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/account/comments">
+            <CommentOutlined />
+            My Feedbacks
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/account/recommendation">
+            <NotificationOutlined />
+            New Recommendations
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="Logout" onClick={() => this.handleLogoutClicked()}>
+          <Link to="/">
+            <LogoutOutlined />
+            Log out
+          </Link>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+  handleLogoutClicked() {
+    this.props.signOut();
+  }
 
-const sideButtons = (props) => {
-  return (
-    <div className={classes.SideButtons}>
-      <LogoutButton></LogoutButton>
-      {props.isSignedIn ? (
-        <DropdownContainer
-          title={"ACCOUNT"}
-          icon={<UserOutlined />}
-          list={profileMenu}
-        />
-      ) : (
-        <ButtonSecondary
-          title={"LOGIN"}
-          icon={<BookOutlined />}
-          onClick={() => props.history.push("/login")}
-        ></ButtonSecondary>
-      )}
-      <ButtonPrimary icon={<HeartOutlined />} title={"LIST"} />
-      <ButtonPrimary icon={<ShoppingCartOutlined />} title={"CART"} />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className={classes.SideButtons}>
+        {this.props.isSignedIn ? (
+          <DropdownContainer
+            title={"ACCOUNT"}
+            icon={<UserOutlined />}
+            list={this.profileMenu()}
+          />
+        ) : (
+          <ButtonSecondary
+            title={"LOGIN"}
+            icon={<BookOutlined />}
+            onClick={() => this.props.history.push("/login")}
+          ></ButtonSecondary>
+        )}
+        <ButtonPrimary icon={<HeartOutlined />} title={"LIST"} />
+        <ButtonPrimary icon={<ShoppingCartOutlined />} title={"CART"} />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -84,5 +90,5 @@ const mapStateToProps = (state) => {
 };
 
 export default withRouter(
-  connect(mapStateToProps, { signIn, signOut })(sideButtons)
+  connect(mapStateToProps, { signIn, signOut })(SideButtons)
 );
