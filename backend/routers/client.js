f55dad0761034d1078f53a8b7d0c18fb1b256880
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2020-12-05 14:42:31 +0300

    Add RUD functonality for models Customer and Vendor

--- backend/routers/client.js
+++ backend/routers/client.js
@@ -1,27 +1,27 @@
 const ClientController = require("../controllers/authClient");
 const RequestHelper = require("./../util/requestHelper");
 
-const rootPath = "/:clientType/";
+const rootPath = "/:clientType";
 
 module.exports = function (server) {
   server.post(`${rootPath}login`, ClientController.loginController, RequestHelper.returnResponse);
   server.get(
-    `${rootPath}verifyEmail`,
+    `${rootPath}/verifyEmail`,
     ClientController.verifyEmailController,
     RequestHelper.returnResponse
   );
   server.post(
-    `${rootPath}changePassword`,
+    `${rootPath}/changePassword`,
     ClientController.changePasswordController,
     RequestHelper.returnResponse
   );
   server.post(
-    `${rootPath}forgotPassword`,
+    `${rootPath}/forgotPassword`,
     ClientController.forgotPasswordController,
     RequestHelper.returnResponse
   );
   server.post(
-    `${rootPath}resetPassword`,
+    `${rootPath}/resetPassword`,
     ClientController.resetPasswordController,
     RequestHelper.returnResponse
   );
--- backend/routers/vendor.js
+++ backend/routers/vendor.js
@@ -1,8 +1,30 @@
 const VendorController = require("../controllers/vendor");
 const RequestHelper = require("./../util/requestHelper");
 
-const rootPath = "/vendor/";
+const rootPath = "/vendor";
 
 module.exports = function (server) {
-  server.post(`${rootPath}signup`, VendorController.signupController, RequestHelper.returnResponse);
+  server.post(
+    `${rootPath}/signup`,
+    VendorController.signupController,
+    RequestHelper.returnResponse
+  );
+
+  server.get(`${rootPath}`, VendorController.getAllVendorsController, RequestHelper.returnResponse);
+
+  server.get(
+    `${rootPath}/:id`,
+    VendorController.getOneVendorController,
+    RequestHelper.returnResponse
+  );
+  server.patch(
+    `${rootPath}/:id`,
+    VendorController.updateOneVendorController,
+    RequestHelper.returnResponse
+  );
+  server.delete(
+    `${rootPath}/:id`,
+    VendorController.deleteOneVendorController,
+    RequestHelper.returnResponse
+  );
 };
