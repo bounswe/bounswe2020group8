bb18abf27ea7cc2f6c236e1b170ce8948178ea7f
Öncel Keleş <31131763+oncelkeles@users.noreply.github.com>
2020-12-12 00:16:15 +0300

    Update UserAccounts.js
    fix class name

--- web/carousel/src/screens/AdminAccount/UserAccounts.js
+++ web/carousel/src/screens/AdminAccount/UserAccounts.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import UserAccountsComponent from "../../components/Admin/UserAccounts/UserAccountsComponent";
 
-export default class Categories extends Component {
+export default class UserAccounts extends Component {
   render() {
     return (
       <div className="Admin-UserAccounts">
