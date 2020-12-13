273f5f59289da938e599d7bf0926d1e10693c808
Burak Berk <burakberkozer@gmail.com>
2020-12-13 19:17:08 +0300

    customer model updated according to class diagram.1

--- backend/models/customer.js
+++ backend/models/customer.js
@@ -9,15 +9,50 @@ var shoppingList = {
     wishedProducts: { type: String }, // This will be product object later on
 }
 
+var order = {
+  customerID: {type: String},
+  product:  {type: String},
+  id: {type: String},
+  email: {type: String},
+  shippingAddress: {type: adress},
+  billingAddress: {type: adress},
+  creditCard: {type: String},
+  shippingInfo: {type: String},
+  refundProcess: {type: String},
+}
+
+var cart = {
+  userID: {type: String},
+  productsIn: {type: String},
+  productAmount: {type: Integer},
+}
+
+var address = {
+  addressName: {type: String},
+  name: {type: String},
+  addressLine: {type: String},
+  city: {type: String},
+  state: {type: String},
+  zipCode: {type: String},
+  phone: {type: String},
+}
+
+var creditCard = {
+  creditCardNumber = {type: String},
+  creditCardCvc = {type: String},
+  creditCardData = {type: String},
+  creditCardName = {type: String}
+}
+
 var customerSchema = new Schema(
   {
     shoppingLists: [shoppingList],
-    orders: { type: String },
-    cart: { type: String },
-    addresses: { type: String },
+    orders: [order],
+    cart: { type: cart },
+    addresses: [adress],
     phoneNumber: { type: String },
     birthday: { type: String },
-    creditCards: { type: String },
+    creditCards: [creditCard],
   },
   { collection: "Clients" }
 );
