098b057649faa9aebd979774b34db44f7f2b1dbc
Burak Berk <burakberkozer@gmail.com>
2020-12-13 19:20:36 +0300

    vendor model updated according to class diagram.

--- backend/models/vendor.js
+++ backend/models/vendor.js
@@ -3,13 +3,23 @@ const ClientBase = require("./client");
 
 const Schema = mongoose.Schema;
 
+var address = {
+  addressName: {type: String},
+  name: {type: String},
+  addressLine: {type: String},
+  city: {type: String},
+  state: {type: String},
+  zipCode: {type: String},
+  phone: {type: String},
+}
+
 var vendorSchema = new Schema(
   {
     companyName: { type: String },
     companyDomainName: { type: String },
     aboutCompany: { type: String },
     IBAN: { type: String },
-    address: { type: String },
+    address: { type: address },
     location: { type: String },
   },
   { collection: "Clients" }
