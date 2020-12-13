15014c75df545572edc43cc5ca37ce1923d1bea5
Burak Berk <burakberkozer@gmail.com>
2020-12-13 20:26:38 +0300

    updated admin model according to class diagram and minor updates on vendor model.

--- backend/models/vendor.js
+++ backend/models/vendor.js
@@ -17,11 +17,45 @@ var location = {
   location: {type: String},
 }
 
+var photo = {
+  id: {type: String},
+  path: {type: String},
+}
+
+var comment = {
+  body: {type: String},
+  rating: {type: Number},
+  ownerId: {type: String},
+  date: {type: Date},
+  id: {type: String},
+}
+
+var product = {
+  id: {type: String},
+  vendors: {},
+  title: {type: String},
+  description: {type: String},
+  amountLeft: {type: Number},
+  price: {type: Number},
+  rating: {type: Number},
+  numberOfRatings: {type: Number},
+  comments: [comment],
+  photos: [photo],
+  brand: {type: String},
+  shipmentPrice: {type: Number},
+  soldAmount: {type: Number},
+  releaseDate: {type: String},
+  cargoCompany: {type: String},
+  category: {type: String},
+  isConfirmed: {type: Boolean},
+}
+
 var vendorSchema = new Schema(
   {
     companyName: { type: String },
     companyDomainName: { type: String },
     aboutCompany: { type: String },
+    products: [product],
     IBAN: {type: String },
     address: {type: address },
     location: {type: String },
