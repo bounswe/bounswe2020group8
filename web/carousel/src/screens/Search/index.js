f0eac1a60735dcb68201f400e79ba0ffea0b99fd
Afra Akbaş <akbasafra.aa@gmail.com>
2020-12-13 18:43:46 +0300

    Search page sidebar created

--- web/carousel/src/screens/Search/index.js
+++ web/carousel/src/screens/Search/index.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
+import { Layout, Menu } from "antd";
+import { withRouter } from "react-router";
 
-export class Search extends Component {
+const { Content, Sider } = Layout;
+
+class Search extends Component {
+  renderSideBar() {
+    return (
+      <Sider className="site-layout-background" width={250}>
+        <Menu mode="inline" style={{ height: "100%" }}>
+          <Menu.Item>Menu 1</Menu.Item>
+        </Menu>
+      </Sider>
+    );
+  }
+
+  renderContent() {
+    return (
+      <Content style={{ padding: "0 24px", minHeight: 280 }}>Content</Content>
+    );
+  }
+
   render() {
-    return <div>Search</div>;
+    return (
+      <div>
+        <Layout>
+          <Content style={{ padding: "0 50px", zIndex: 10 }}>
+            <Layout
+              className="site-layout-background"
+              style={{ padding: "24px 0" }}
+            >
+              {this.renderSideBar()}
+              {this.renderContent()}
+            </Layout>
+          </Content>
+        </Layout>
+      </div>
+    );
   }
 }
 
-export default Search;
+export default withRouter(Search);
