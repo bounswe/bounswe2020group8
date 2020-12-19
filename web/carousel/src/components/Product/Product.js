e03615c9fee0a21de11875b006a19ca086fe4da0
Yasin <ykaya932@gmail.com>
2020-12-20 02:28:20 +0300

    Add Product Photos

--- web/carousel/src/components/Product/Product.js
+++ web/carousel/src/components/Product/Product.js
@@ -1,14 +1,23 @@
-import React, {useContext, useState, useRef} from "react";
+import React, { useContext, useState, useRef } from "react";
 import classes from "./Product.module.css";
 import SectionToggle from "./ProductInfo/SectionToggle";
 import ProductHeader from "./ProductHeader/ProductHeader";
 import ProductActions from "./ProductActions/ProductActions";
 import scrollIntoView from "scroll-into-view-if-needed";
+import ProductPhotoCarousel from "./ProductPhotoCarousel/ProductPhotoCarousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 const Product = (props) => {
-  const[infoSection, setInfoSection] = useState("features");
+  const [infoSection, setInfoSection] = useState("features");
 
   const sectionRef = useRef(null);
+
+  const photoUrls = [
+    "https://images-na.ssl-images-amazon.com/images/I/51QWDcpe3JL._AC_SL1024_.jpg",
+    "https://images-na.ssl-images-amazon.com/images/I/51em%2BfFP9nL._AC_SL1024_.jpg",
+    "https://images-na.ssl-images-amazon.com/images/I/61sZFcEZG5L._AC_SL1500_.jpg",
+    "https://images-na.ssl-images-amazon.com/images/I/513LaFkyePL._AC_SL1024_.jpg",
+  ];
 
   const scrollToInfo = (section) => {
     setInfoSection(section);
@@ -16,37 +25,40 @@ const Product = (props) => {
     window.scrollTo({
       left: 0,
       top: 480,
-      behavior:'smooth'
+      behavior: "smooth",
     });
     //sectionRef.current.scrollIntoView({behavior: "smooth", block: "start", inline:"start"});
     setTimeout(() => {
       setInfoSection("");
-    },500);
-  }
+    }, 500);
+  };
 
-  return(
+  return (
     <div className={classes.ProductPage}>
-      <div style={{height:"60px"}}>we may display the path here if we want to. e.g.: Ana Sayfa > Telefonlar & Aksesuarlar > Cep Telefonları >
-        Android Telefonlar > Samsung Android Telefonlar</div>
+      <div style={{ height: "60px" }}>
+        we may display the path here if we want to. e.g.: Ana Sayfa > Telefonlar
+        & Aksesuarlar > Cep Telefonları > Android Telefonlar > Samsung Android
+        Telefonlar
+      </div>
       <div className={classes.ProductOverview}>
         <div className={classes.ProductPhotos}>
-          Product Photos
+          <ProductPhotoCarousel photoUrls={photoUrls} />
         </div>
-        <div style={{width:"10px"}}/>
+        <div style={{ width: "10px" }} />
         <div className={classes.ProductRight}>
           <ProductHeader
             clickReviews={() => scrollToInfo("comments")}
-            clickFeatures={() => scrollToInfo("features")}/>
-          <ProductActions clickSellers={() => scrollToInfo("sellers")}/>
+            clickFeatures={() => scrollToInfo("features")}
+          />
+          <ProductActions clickSellers={() => scrollToInfo("sellers")} />
         </div>
       </div>
-      <div  style={{height:"40px"}}/>
+      <div style={{ height: "40px" }} />
       <div className={classes.ProductInfo} ref={sectionRef}>
-        <SectionToggle section={infoSection}/>
-
+        <SectionToggle section={infoSection} />
       </div>
     </div>
   );
-}
+};
 
 export default Product;
---
+++ web/carousel/src/components/Product/ProductPhotoCarousel/ProductPhotoCarousel.js
@@ -0,0 +1,52 @@
+import Image from "react-image-resizer";
+
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import ReactImageZoom from "react-image-zoom";
+
+const ProductPhotoCarousel = (props) => {
+  const renderDotsItem = ({ isActive, activeIndex }) => {
+    return (
+      <div
+        style={{
+          height: 50,
+          width: 50,
+          margin: 2,
+          border: isActive ? "1px solid #afafaf" : "0px",
+          borderRadius: "5px",
+          padding: 2,
+        }}
+      >
+        <Image src={props.photoUrls[activeIndex]} width={46} height={46} />
+      </div>
+    );
+  };
+
+  return (
+    <AliceCarousel
+      autoHeight={true}
+      autoWidth={true}
+      renderDotsItem={renderDotsItem}
+      disableButtonsControls
+    >
+      {props.photoUrls.map((url) => {
+        return (
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              textAlign: "center",
+              alignContent: "center",
+
+              backgroundColor: "white",
+            }}
+          >
+            <Image src={url} width={500} height={300} />
+          </div>
+        );
+      })}
+    </AliceCarousel>
+  );
+};
+
+export default ProductPhotoCarousel;
