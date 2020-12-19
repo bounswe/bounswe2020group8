1096077d1c0a38c555a4850b6173157e0b326758
Ahmet Yiğit Gedik <ahmetyigitgedik@gmail.com>
2020-12-19 16:53:26 +0300

    api connections have been implemented to the category section of the admin panel

--- web/carousel/src/components/Admin/Category/CategoriesComponent.js
+++ web/carousel/src/components/Admin/Category/CategoriesComponent.js
@@ -2,18 +2,62 @@ import { Tag, Input } from "antd";
 import { TweenOneGroup } from "rc-tween-one";
 import { PlusOutlined } from "@ant-design/icons";
 import React from "react";
+import axios from "axios";
 import "./CategoriesComponent.module.css";
+
+const apiBaseUrl = "http://54.165.207.44:8080/";
+const TOKEN = localStorage.getItem("token");
 
 export default class CategoriesComponent extends React.Component {
   state = {
-    tags: ["Category 1", "Category 2", "Category 3"],
+    tags: [],
     inputVisible: false,
     inputValue: "",
+    categoryIdDic: {},
   };
 
+  async componentWillMount() {
+    var config = {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+    await axios
+      .get(apiBaseUrl + "category", config)
+      .then((response) => {
+        console.log(response.data);
+        var categoryList = response.data.data;
+        var category_arr = [];
+        var category_id_dic = {};
+        categoryList.forEach((element) => {
+          if (typeof element["name"] != "undefined") {
+            category_arr.push(element["name"]);
+            category_id_dic[element["name"]] = element["_id"];
+          }
+        });
+        console.log(category_id_dic);
+        this.setState({ tags: category_arr });
+        this.setState({ categoryIdDic: category_id_dic });
+      })
+      .catch((err, response) => {
+        console.log(err);
+        this.setState({ isError: true });
+      });
+  }
   handleClose = (removedTag) => {
+    var id = this.state.categoryIdDic[removedTag];
+    console.log(id);
+    var config = {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+    axios
+      .delete(apiBaseUrl + "category/" + id, config)
+      .then((response) => {})
+      .catch((err, response) => {});
     const tags = this.state.tags.filter((tag) => tag !== removedTag);
-    console.log(tags);
+    delete this.state.categoryIdDic[removedTag];
     this.setState({ tags });
   };
 
@@ -27,6 +71,20 @@ export default class CategoriesComponent extends React.Component {
 
   handleInputConfirm = () => {
     const { inputValue } = this.state;
+    var postData = {
+      name: inputValue,
+    };
+    var config = {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+    axios
+      .post(apiBaseUrl + "category", postData, config)
+      .then((response) => {
+        this.state.categoryIdDic[inputValue] = response.data.data["_id"];
+      })
+      .catch((err, response) => {});
     let { tags } = this.state;
     if (inputValue && tags.indexOf(inputValue) === -1) {
       tags = [...tags, inputValue];
