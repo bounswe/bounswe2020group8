297a6893921e89ec92a0fc665a789e6368fc4ea6
afraarslan <akbasafra.aa@gmail.com>
2020-12-25 00:28:14 +0300

    Add Cart and List screens

--- web/carousel/src/screens/Account/Cart.js
+++ web/carousel/src/screens/Account/Cart.js
@@ -1,16 +1,114 @@
-import React from "react";
-import { Layout, Menu, Divider } from "antd";
+import React, { useState } from "react";
+import { Layout, Divider } from "antd";
 import ButtonPrimary from "../../components/UI/ButtonPrimary/ButtonPrimary";
+import ButtonSecondary from "../../components/UI/ButtonSecondary/ButtonSecondary";
+import Image from "react-image-resizer";
+import { DeleteOutlined } from "@ant-design/icons";
+import { useHistory, withRouter } from "react-router-dom";
 
 const { Content, Sider } = Layout;
-const x = "55$";
+const productListDemo = [
+  {
+    imageUrl:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp16touch-space-select-201911_GEO_TR?wid=892&hei=820&&qlt=80&.v=1582326712648",
+    name: "Macbook Pro 16 inch",
+    price: 2199.99,
+    vendorName: "AA",
+  },
+  {
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41GGPRqTZtL._AC_.jpg",
+    name: "PlayStation 4 Pro 1TB",
+    price: 399.99,
+    vendorName: "AA",
+  },
+  {
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/61BhxjpQn6L._AC_SL1500_.jpg",
+    name: "Arlo VMC2030-100NAS Essential Spotlight Camera",
+    price: 99.99,
+    vendorName: "AA",
+  },
+  {
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/318TG3aNKpL._AC_US218_.jpg",
+    name: "Introducing Fire TV Stick Lite with Alexa Voice Remote Lite",
+    price: 18.99,
+    vendorName: "AA",
+  },
+];
+const productPrice = 123.43;
+const shipmentPrice = 25.5;
 
-export default function Cart() {
-  return (
-    <Layout>
-      <Content style={{ padding: "0 50px" }}></Content>
-      <Layout className="site-layout-background" style={{ padding: "24px 0" }}>
-        <Content style={{ padding: "0 24px", minHeight: 280 }}>Content</Content>
+const Cart = () => {
+  const history = useHistory();
+  const [productList, setproductList] = useState(productListDemo);
+
+  const onShopClicked = () => {
+    history.push("/");
+  };
+
+  const handleDeleteClicked = ({ name }) => {
+    const newList = productList.filter((item) => item.name !== name);
+    setproductList(newList);
+  };
+
+  const handleConfirmClicked = () => {
+    console.log("Confirm");
+  };
+
+  function ProductContent(productList = []) {
+    return (
+      <div style={{ fontSize: 24, fontWeight: "bold", color: "#d33a09" }}>
+        My Cart
+        {productList.map((product) => (
+          <>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: 20,
+                height: 100,
+                borderColor: "gray",
+                border: "1px solid",
+                color: "navy",
+              }}
+            >
+              <div>
+                <Image height={70} width={70} src={product.imageUrl} />
+              </div>
+              <div
+                style={{
+                  fontWeight: "normal",
+                  marginRight: "auto",
+                  padding: 20,
+                }}
+              >
+                <div style={{ fontSize: 16 }}>{product.name}</div>
+                <div style={{ fontSize: 12 }}>Vendor: {product.vendorName}</div>
+              </div>
+              <div style={{ marginLeft: "auto", padding: 50 }}>
+                <div>{product.price}$</div>
+              </div>
+              <div>
+                <DeleteOutlined
+                  style={{ fontSize: 20 }}
+                  onClick={() => handleDeleteClicked(product)}
+                />
+              </div>
+            </div>
+            <Divider />
+          </>
+        ))}
+      </div>
+    );
+  }
+
+  function PriceSider() {
+    return (
+      <div>
         <Sider
           className="site-layout-background"
           width={250}
@@ -37,16 +135,18 @@ export default function Cart() {
               <Divider style={{ width: 220 }} />
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <div style={{ fontSize: 16 }}>Product price</div>
-                <div style={{ fontSize: 16 }}>123.64</div>
+                <div style={{ fontSize: 16 }}>{productPrice}</div>
               </div>
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <div style={{ fontSize: 16 }}>Shipment price</div>
-                <div style={{ fontSize: 16 }}>25.5 </div>
+                <div style={{ fontSize: 16 }}>{shipmentPrice}</div>
               </div>
               <Divider style={{ width: 220 }} />
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <div style={{ fontSize: 16 }}>Total price</div>
-                <div style={{ fontSize: 16 }}>165</div>
+                <div style={{ fontSize: 16 }}>
+                  {productPrice + shipmentPrice}
+                </div>
               </div>
             </div>
             <Divider style={{ width: 220 }} />
@@ -54,12 +154,34 @@ export default function Cart() {
               <ButtonPrimary
                 title="Confirm"
                 style={{ width: 150 }}
-                onClick={() => console.log("clicked")}
+                onClick={() => handleConfirmClicked()}
               />
             </div>
           </div>
         </Sider>
+      </div>
+    );
+  }
+
+  return (
+    <Layout>
+      <Layout className="site-layout-background" style={{ padding: "24px 0" }}>
+        <Content
+          style={{
+            padding: "0 24px",
+            minHeight: 280,
+          }}
+        >
+          {ProductContent(productList)}
+          <ButtonSecondary
+            title="Go back to Shopping"
+            onClick={() => onShopClicked()}
+          />
+        </Content>
+        {PriceSider()}
       </Layout>
     </Layout>
   );
-}
+};
+
+export default withRouter(Cart);
--- web/carousel/src/screens/Account/List.js
+++ web/carousel/src/screens/Account/List.js
@@ -1,5 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Divider, Badge } from "antd";
+import ButtonPrimary from "../../components/UI/ButtonPrimary/ButtonPrimary";
+import ButtonSecondary from "../../components/UI/ButtonSecondary/ButtonSecondary";
+import Image from "react-image-resizer";
+import { DeleteOutlined } from "@ant-design/icons";
+import { HeartOutlined } from "@ant-design/icons";
+import { useHistory, withRouter } from "react-router-dom";
 
-export default function List() {
-  return <div>List</div>;
-}
+const { Content } = Layout;
+const productListDemo = [
+  {
+    imageUrl:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp16touch-space-select-201911_GEO_TR?wid=892&hei=820&&qlt=80&.v=1582326712648",
+    name: "Macbook Pro 16 inch",
+    price: 2199.99,
+    vendorName: "AA",
+  },
+  {
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41GGPRqTZtL._AC_.jpg",
+    name: "PlayStation 4 Pro 1TB",
+    price: 399.99,
+    vendorName: "AA",
+  },
+];
+
+const List = () => {
+  const history = useHistory();
+  const [productList, setproductList] = useState(productListDemo);
+  const handleShopClicked = () => {
+    history.push("/");
+  };
+
+  const handleDeleteClicked = ({ name }) => {
+    const newList = productList.filter((item) => item.name !== name);
+    setproductList(newList);
+  };
+
+  const handleCartClicked = () => {
+    history.push("/account/cart");
+  };
+
+  function ProductContent(productList = []) {
+    return (
+      <div style={{ fontSize: 24, fontWeight: "bold", color: "#d33a09" }}>
+        My List
+        {productList.map((product) => (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Badge.Ribbon text={<HeartOutlined />} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: 20,
+                height: 100,
+                borderColor: "gray",
+                border: "1px solid",
+                color: "navy",
+              }}
+            >
+              <div>
+                <Image height={70} width={70} src={product.imageUrl} />
+              </div>
+              <div style={{ fontWeight: "normal" }}>
+                <div style={{ fontSize: 16 }}>{product.name}</div>
+                <div style={{ fontSize: 12 }}>Vendor: {product.vendorName}</div>
+              </div>
+              <div>
+                <div>{product.price}$</div>
+              </div>
+              <div>
+                <DeleteOutlined
+                  style={{ fontSize: 20 }}
+                  onClick={() => handleDeleteClicked(product)}
+                />
+              </div>
+              <div>
+                <ButtonPrimary
+                  title="Add to Cart"
+                  style={{ width: 120, height: 50, fontSize: 16 }}
+                  onClick={() => handleCartClicked()}
+                />
+              </div>
+            </div>
+            <Divider />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <Layout>
+      <Layout className="site-layout-background" style={{ padding: "24px 0" }}>
+        <Content
+          style={{
+            padding: "0 24px",
+            minHeight: 280,
+          }}
+        >
+          {ProductContent(productList)}
+          <ButtonSecondary
+            title="Go back to Shopping"
+            onClick={() => handleShopClicked()}
+          />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default withRouter(List);
