14f38b2a873a862341ebc64173903091bf5a7467
Burak Berk Özer <burakberkozer@gmail.com>
2020-12-26 20:57:38 +0300

    rerouted shoppingCart

--- backend/routers/customer.js
+++ backend/routers/customer.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const CustomerController = require("../controllers/customer");
+const ShoppingCartController = require("../controllers/shoppingCart");
 const RequestHelper = require("./../util/requestHelper");
 const authController = require("../controllers/authClient");
 const router = express.Router();
@@ -32,5 +33,22 @@ router
   .get(CustomerController.getOneCustomerController, RequestHelper.returnResponse)
   .patch(CustomerController.updateOneCustomerController, RequestHelper.returnResponse)
   .delete(CustomerController.deleteOneCustomerController, RequestHelper.returnResponse);
+
+  router.post(
+    "/update",
+    ShoppingCartController.updateShoppingCartController,
+    RequestHelper.returnResponse
+  );
+  router.post(
+    "/delete",
+    ShoppingCartController.deleteFromShoppingCartController,
+    RequestHelper.returnResponse
+  );
+  router.get(
+    "/reset",
+    ShoppingCartController.resetShoppingCartController,
+    RequestHelper.returnResponse
+  );
+  router.get("/get", ShoppingCartController.getShoppingCartController, RequestHelper.returnResponse);
 
 module.exports = router;
