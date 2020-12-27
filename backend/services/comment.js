8a8555d97ad9dc9c0c2e50480f919219ceac910d
oztasozgurcan <oztas.ozgurcan@gmail.com>
2020-12-27 19:55:28 +0300

    Added results value of the data

--- backend/services/comment.js
+++ backend/services/comment.js
@@ -2,12 +2,12 @@ const CommentDataAccess = require("../dataAccess/comment");
 
 exports.getCommentsService = async function (mainProductId, customerId) {
   let comments = await CommentDataAccess.findPidAndCid(mainProductId, customerId);
-  return { data: comments };
+  return { results: comments.length, data: comments };
 };
 
 exports.getAlCommentsService = async function (mainProductId) {
   let comments = await CommentDataAccess.findPid(mainProductId);
-  return { data: comments };
+  return { results: comments.length, data: comments };
 };
 
 exports.createOneCommentService = async function (req) {
@@ -22,15 +22,15 @@ exports.createOneCommentService = async function (req) {
       text,
     })
   ).toObject();
-  return { data: newComment };
+  return { results: 1, data: newComment };
 };
 
 exports.updateOneCommentService = async function (_id, text) {
   let updatedComment = await CommentDataAccess.findByIdAndUpdate(_id, text);
-  return { data: updatedComment };
+  return { results: 1, data: updatedComment };
 };
 
 exports.deleteOneCommentService = async function (_id) {
   let deletedComment = await CommentDataAccess.deleteOne(_id);
-  return { data: deletedComment };
+  return { results: 1, data: deletedComment };
 };
