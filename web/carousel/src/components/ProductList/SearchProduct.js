655394d8c50ffb95e1db9127dd2b535b90ec6d1e
Yasin <ykaya932@gmail.com>
2020-12-28 18:03:03 +0300

    remove

--- web/carousel/src/components/ProductList/SearchProduct.js
+++ web/carousel/src/components/ProductList/SearchProduct.js
@@ -2,18 +2,21 @@ import classes from "./SearchProduct.module.css";
 import ButtonPrimary from "../UI/ButtonPrimary/ButtonPrimary";
 import Image from "react-image-resizer";
 import FixedDiv from "../UI/FixedDiv/FixedDiv";
+import { withRouter } from "react-router-dom";
 
 import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 import { Rate } from "antd";
 
 import { useState } from "react";
 
-const SearchProduct = ({ product }) => {
+const SearchProduct = (props) => {
+  const product = props.product;
   const [liked, setLiked] = useState(false);
 
   const toggleLiked = () => {
     setLiked(!liked);
   };
+
   const { mainProduct, photos, minPrice } = product;
   const { title, rating, numberOfRating } = mainProduct[0];
   return (
@@ -36,41 +39,49 @@ const SearchProduct = ({ product }) => {
         </div>
       </FixedDiv>
 
-      <FixedDiv width={350} height={250} margin={"20px 0px"}>
-        <Image height={250} width={350} src={photos[0]} />
-      </FixedDiv>
       <div
-        style={{
-          width: "350px",
-          height: "25px",
-          textAlign: "left",
-          paddingLeft: "20px",
-        }}
+        onClick={() =>
+          withRouter(props.history.push(`/product/${product._id}`))
+        }
       >
-        <span>
-          <Rate
-            disabled
-            allowHalf
-            defaultValue={rating}
-            style={{ fontSize: 15 }}
-          />
-        </span>
-        <span style={{ marginLeft: 10, color: "#afafaf" }}>
-          ({numberOfRating})
-        </span>
+        <FixedDiv width={350} height={250} margin={"20px 0px"}>
+          <Image height={250} width={350} src={photos[0]} />
+        </FixedDiv>
+        <div
+          style={{
+            width: "350px",
+            height: "25px",
+            textAlign: "left",
+            paddingLeft: "20px",
+          }}
+        >
+          <span>
+            <Rate
+              disabled
+              allowHalf
+              defaultValue={rating}
+              style={{ fontSize: 15 }}
+            />
+          </span>
+          <span style={{ marginLeft: 10, color: "#afafaf" }}>
+            ({numberOfRating})
+          </span>
+        </div>
+        <FixedDiv width={350} height={45} margin={"10px 0px 0px 0px"}>
+          <p>{title}</p>
+        </FixedDiv>
+        <FixedDiv width={350} height={15} margin={"0px 0px 30px 0px"}>
+          <p className={classes.Price}>
+            <b>${minPrice}</b>
+          </p>
+        </FixedDiv>
       </div>
-      <FixedDiv width={350} height={45} margin={"10px 0px 0px 0px"}>
-        <p>{title}</p>
-      </FixedDiv>
-      <FixedDiv width={350} height={15} margin={"0px 0px 30px 0px"}>
-        <p className={classes.Price}>
-          <b>${minPrice}</b>
-        </p>
-      </FixedDiv>
 
-      <button className={classes.Button}>Add to Cart</button>
+      <button className={classes.Button} onClick={() => alert("Add to Cart")}>
+        Add to Cart
+      </button>
     </div>
   );
 };
 
-export default SearchProduct;
+export default withRouter(SearchProduct);
