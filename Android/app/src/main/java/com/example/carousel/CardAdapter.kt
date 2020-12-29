52f6278a08a8b44a6b59801d9624c82a887ce2e0
Göksu Başer <goksubaser@gmail.com>
2020-12-29 03:21:36 +0300

    Card delete functionality is added

--- Android/app/src/main/java/com/example/carousel/CardAdapter.kt
+++ Android/app/src/main/java/com/example/carousel/CardAdapter.kt
@@ -7,6 +7,11 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.carousel.map.ApiCaller
+import com.example.carousel.map.ApiClient
+import com.example.carousel.pojo.DataCustomerMe
+import com.example.carousel.pojo.ExampleObject
+import com.example.carousel.pojo.ResponseCustomerMe
 
 class CardAdapter(
     private var cardList: ArrayList<Card>,
@@ -39,6 +44,84 @@ class CardAdapter(
     override fun onBindViewHolder(holder: CardAdapter.ViewHolder, position: Int) {
         val temp = cardList[position].creditCardNumber
         holder.cardName.text = "**** **** **** "+temp.subSequence(temp.length-4,temp.length)
+        holder.deleteButton.setOnClickListener{
+            deleteCard(position)
+        }
     }
 
+    private fun deleteCard(position: Int) {
+        var id: String
+        var name: String
+        var lastName: String
+        var email: String
+        var isSuspended: Boolean
+        var isActive: Boolean
+        var shoppingLists: List<List<Product>>?
+        var orders: List<ExampleObject>?
+        var cart: List<ExampleObject>?
+        var addresses: List<Address>?
+        var telephoneNumber: String?
+        var birthday: String?
+        var creditCards: List<Card>?
+
+        val apiCaller: ApiCaller<ResponseCustomerMe> = ApiCaller(activity)
+        apiCaller.Caller = ApiClient.getClient.customerMe()
+        apiCaller.Success = { it ->
+            if (it != null) {
+                activity?.runOnUiThread(Runnable { //Handle UI here
+                    id = it.data.id
+                    name = it.data.name
+                    lastName = it.data.lastName
+                    email = it.data.email
+                    isSuspended = it.data.isSuspended
+                    isActive = it.data.isActive
+                    shoppingLists = it.data.shoppingLists
+                    orders = it.data.orders
+                    cart = it.data.cart
+                    addresses = it.data.addresses
+                    telephoneNumber = it.data.telephoneNumber
+                    birthday = it.data.birthday
+                    creditCards = it.data.creditCards
+                    var tempCreditCards = creditCards?.toMutableList()
+                    if (tempCreditCards != null) {
+                        tempCreditCards.removeAt(position)
+                    }
+                    creditCards = tempCreditCards?.toList()
+
+                    var newData = DataCustomerMe(
+                        id,
+                        name,
+                        lastName,
+                        email,
+                        isSuspended,
+                        isActive,
+                        shoppingLists,
+                        orders,
+                        cart,
+                        addresses,
+                        telephoneNumber,
+                        birthday,
+                        creditCards
+                    )
+                    val apiCallerPatch: ApiCaller<ResponseCustomerMe> = ApiCaller(activity)
+                    apiCallerPatch.Caller = ApiClient.getClient.customerUpdate(newData)
+                    apiCallerPatch.Success = { it ->
+                        if (it != null) {
+                            activity?.runOnUiThread(Runnable { //Handle UI here
+                                val fragment = Settings()
+                                activity?.supportFragmentManager?.beginTransaction()
+                                    ?.replace(R.id.fragment_account_page, fragment)
+                                    ?.commit()
+                            })
+                        }
+                    }
+                    apiCallerPatch.Failure = {}
+                    apiCallerPatch.run()
+
+                })
+            }
+        }
+        apiCaller.Failure = {}
+        apiCaller.run()
+    }
 }
