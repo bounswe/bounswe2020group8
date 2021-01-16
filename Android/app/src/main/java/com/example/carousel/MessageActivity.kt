43bf37fdb7a1efa461fd0f30b28999c8b26ac0c7
OnurEnginer <onurenginer1@gmail.com>
2021-01-16 22:26:02 +0300

    Fixed a view issue with messages

--- Android/app/src/main/java/com/example/carousel/MessageActivity.kt
+++ Android/app/src/main/java/com/example/carousel/MessageActivity.kt
@@ -2,6 +2,7 @@ package com.example.carousel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.carousel.pojo.VendorSpecifics
 import com.xwray.groupie.GroupieAdapter
@@ -9,10 +10,12 @@ import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 
 import kotlinx.android.synthetic.main.activity_message.*
+import kotlinx.android.synthetic.main.activity_message.view.*
 import kotlinx.android.synthetic.main.other_chat_item.view.*
 import kotlinx.android.synthetic.main.product_view.view.*
 
 class MessageActivity : AppCompatActivity() {
+    lateinit var groupieAdapter: GroupieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
@@ -21,19 +24,21 @@ class MessageActivity : AppCompatActivity() {
         }
         topAppBar.title = "Messages"
 
-        val groupieAdapter = GroupieAdapter()
+        groupieAdapter = GroupieAdapter()
 
         groupieAdapter.add(MessageFromItem(Message("Hello friend how are you doing in this blessed day?")))
         groupieAdapter.add(MessageToItem(Message("Hi")))
         groupieAdapter.add(MessageFromItem(Message("Bye")))
         groupieAdapter.add(MessageToItem(Message("Bye!")))
 
-
         message_view.apply{
             layoutManager = LinearLayoutManager(this@MessageActivity, LinearLayoutManager.VERTICAL, false)
             adapter = groupieAdapter
         }
 
+    }
+    fun sendMessage(view: View){
+        groupieAdapter.add(MessageToItem(Message(message_input.editText?.text.toString())))
     }
 }
 
