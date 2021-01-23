42a68f34b2f4b562be1f1882b71d95196ec527b3
nursima çelik <nursima.celik@boun.edu.tr>
2021-01-23 17:48:16 +0300

    Prepare Search fragment to testing by dividing to functions

--- Android/app/src/main/java/com/example/carousel/SearchFragment.kt
+++ Android/app/src/main/java/com/example/carousel/SearchFragment.kt
@@ -2,6 +2,7 @@ package com.example.carousel
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +27,7 @@ import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
 class SearchFragment : Fragment() {
     //private val baseUrl = "http://54.165.207.44:8080"
-    private var lastQuery = "fashion"   // a default query
+    private var lastQuery = ""   // a default query
     private var lastSort = ""
     private lateinit var spinnerAdapter: ArrayAdapter<String>
     private var initializedView = false
@@ -44,8 +45,8 @@ class SearchFragment : Fragment() {
     private val sortOptionsMap = mapOf<String, String>("Lowest Price" to "minPrice", "Highest Price" to "-minPrice", "Best Rating" to "-rating",
         "Most commented" to "-numberOfRatings", "Newest" to "releaseDate")
 
-    private val ratingFilterMap = mapOf<String, Int>("1 star and above" to 1, "2 stars and above" to 2, "3 stars and above" to 3,
-        "4 stars and above" to 4, "5 stars" to 5)
+    private val ratingFilterMap = mapOf<String, Int>("one_star" to 1, "two_stars" to 2, "three_stars" to 3,
+        "four_stars" to 4, "five_stars" to 5)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,9 +57,32 @@ class SearchFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        placecat()
+        placeCategories()
 
-        var sort = ""
+        initSortSpinner()
+
+        initFilterButton()
+
+        /*rating_clear.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            rating_radio_group.clearCheck()
+        })*/
+
+        initApplyButton()
+
+        initSearchView()
+    }
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+
+
+    private fun initSortSpinner() {
 
         val spinner = sort_spinner
         ArrayAdapter.createFromResource(
@@ -83,13 +107,17 @@ class SearchFragment : Fragment() {
                 }
                 else {
                     val str = context?.resources!!.getStringArray(R.array.sort_options).get(pos)
-                    sort = sortOptionsMap[str].toString()
-                    lastSort = sort
+                    lastSort = sortOptionsMap[str].toString()
                     searchCall()
                 }
             }
 
         }
+    }
+
+
+
+    private fun initFilterButton() {
 
         filterButton.setOnClickListener {
             drawer_layout.openDrawer(Gravity.RIGHT);
@@ -102,63 +130,13 @@ class SearchFragment : Fragment() {
         category_container.visibility = View.GONE
         //vendors_container.visibility = View.GONE
 
+        initPriceFilter()
+        initRatingFilter()
+        initColorFilter()
+        initSizeFilter()
+        initBrandFilter()
+        initCategoryFilter()
 
-        price_filter.setOnClickListener {
-            if(expandable_price.visibility == View.GONE) {
-                expandable_price.visibility = View.VISIBLE
-            }
-            else {
-                expandable_price.visibility = View.GONE
-
-            }
-        }
-
-        rating_filter.setOnClickListener {
-            if(expandable_rating.visibility == View.GONE) {
-                expandable_rating.visibility = View.VISIBLE
-            }
-            else {
-                expandable_rating.visibility = View.GONE
-
-            }
-        }
-
-        color_filter.setOnClickListener {
-            if(color_container.visibility == View.GONE) {
-                color_container.visibility = View.VISIBLE
-            }
-            else {
-                color_container.visibility = View.GONE
-
-            }
-        }
-        size_filter.setOnClickListener {
-            if(size_container.visibility == View.GONE) {
-                size_container.visibility = View.VISIBLE
-            }
-            else {
-                size_container.visibility = View.GONE
-
-            }
-        }
-        brand_filter.setOnClickListener {
-            if(brand_container.visibility == View.GONE) {
-                brand_container.visibility = View.VISIBLE
-            }
-            else {
-                brand_container.visibility = View.GONE
-
-            }
-        }
-        category_filter.setOnClickListener {
-            if(category_container.visibility == View.GONE) {
-                category_container.visibility = View.VISIBLE
-            }
-            else {
-                category_container.visibility = View.GONE
-
-            }
-        }
         /*vendors_filter.setOnClickListener {
             if(vendors_container.visibility == View.GONE) {
                 vendors_container.visibility = View.VISIBLE
@@ -168,8 +146,77 @@ class SearchFragment : Fragment() {
 
             }
         }*/
+    }
 
+    private fun initCategoryFilter() {
+        category_filter.setOnClickListener {
+            if(category_container.visibility == View.GONE) {
+                category_container.visibility = View.VISIBLE
+            }
+            else {
+                category_container.visibility = View.GONE
 
+            }
+        }
+    }
+
+    private fun initBrandFilter() {
+        brand_filter.setOnClickListener {
+            if(brand_container.visibility == View.GONE) {
+                brand_container.visibility = View.VISIBLE
+            }
+            else {
+                brand_container.visibility = View.GONE
+
+            }
+        }
+    }
+
+    private fun initSizeFilter() {
+        size_filter.setOnClickListener {
+            if(size_container.visibility == View.GONE) {
+                size_container.visibility = View.VISIBLE
+            }
+            else {
+                size_container.visibility = View.GONE
+
+            }
+        }
+    }
+
+    private fun initColorFilter() {
+        color_filter.setOnClickListener {
+            if(color_container.visibility == View.GONE) {
+                color_container.visibility = View.VISIBLE
+            }
+            else {
+                color_container.visibility = View.GONE
+
+            }
+        }
+    }
+
+    private fun initRatingFilter() {
+        rating_filter.setOnClickListener {
+            if (expandable_rating.visibility == View.GONE) {
+                expandable_rating.visibility = View.VISIBLE
+            } else {
+                expandable_rating.visibility = View.GONE
+
+            }
+        }
+    }
+
+    private fun initPriceFilter() {
+        price_filter.setOnClickListener {
+            if(expandable_price.visibility == View.GONE) {
+                expandable_price.visibility = View.VISIBLE
+            }
+            else {
+                expandable_price.visibility = View.GONE
+
+            }
+        }
         button_0_50.setOnClickListener {
             //button_0_50.setBackgroundColor(Color.parseColor("#FF1FEAD7"))
             min_price.setText("0")
@@ -191,83 +238,17 @@ class SearchFragment : Fragment() {
             min_price.setText("500")
             max_price.setText("")
         }
+    }
 
-        /*rating_clear.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
-            rating_radio_group.clearCheck()
-        })*/
-
-
-
-
+    private fun initApplyButton() {
         apply_button.setOnClickListener {
-            if(min_price.text.toString() != "") {
-                queryMinPrice = "minPrice[gte]=" + min_price.text.toString()
-            }
-            if(max_price.text.toString() != "") {
-                queryMaxPrice = "minPrice[lte]=" + min_price.text.toString()
-            }
-            if(rating_radio_group.getCheckedRadioButtonId() != -1) {
-                val selectedId = rating_radio_group.getCheckedRadioButtonId()
-                val selectedRadioButton = resources.getResourceEntryName(selectedId)
-                val r = ratingFilterMap[selectedRadioButton]
-                queryRating = "rating[gte]=" + r
-
-            }
-            var color_first = true
-            for(i in 0..(color_container.childCount-1)) {
-                val view = color_container.getChildAt(i) as CheckBox
-                if(view.isChecked) {
-                    if(color_first) {
-                        queryColor = "color=" + view.text.toString()
-                        color_first = false
-                    }
-                    else {
-                        queryColor += "," + view.text.toString()
-                    }
-                }
-            }
-
-            var brand_first = true
-            for(i in 0..(brand_container.childCount-1)) {
-                val view = brand_container.getChildAt(i) as CheckBox
-                if(view.isChecked) {
-                    if(brand_first) {
-                        queryBrand = "brand=" + view.text.toString()
-                        brand_first = false
-                    }
-                    else {
-                        queryBrand += "," + view.text.toString()
-                    }
-                }
-            }
-
-            var size_first = true
-            for(i in 0..(size_container.childCount-1)) {
-                val view = size_container.getChildAt(i) as CheckBox
-                if(view.isChecked) {
-                    if(size_first) {
-                        querySize = "size=" + view.text.toString()
-                        size_first = false
-                    }
-                    else {
-                        querySize += "," + view.text.toString()
-                    }
-                }
-            }
-
-            var category_first = true
-            for(i in 0..(category_container.childCount-1)) {
-                val view = category_container.getChildAt(i) as CheckBox
-                if(view.isChecked) {
-                    if(category_first) {
-                        queryCategory = "category=" + view.text.toString()
-                        category_first = false
-                    }
-                    else {
-                        queryCategory += "," + view.text.toString()
-                    }
-                }
-            }
+            determineQueryMinPrice()
+            determineQueryMaxPrice()
+            determineQueryRating()
+            determineQueryColor()
+            determineQueryBrand()
+            determineQuerySize()
+            determineQueryCategory()
 
             /*var vendors_first = true
             for(i in 0..(vendors_container.childCount-1)) {
@@ -286,7 +267,103 @@ class SearchFragment : Fragment() {
             searchCall()
 
         }
+    }
 
+    private fun determineQueryCategory() {
+        var category_first = true
+        for(i in 0..(category_container.childCount-1)) {
+            val view = category_container.getChildAt(i) as CheckBox
+            if(view.isChecked) {
+                if(category_first) {
+                    queryCategory = "category=" + view.text.toString()
+                    category_first = false
+                }
+                else {
+                    queryCategory += "," + view.text.toString()
+                }
+            }
+        }
+    }
+
+    private fun determineQuerySize() {
+        var size_first = true
+        for(i in 0..(size_container.childCount-1)) {
+            val view = size_container.getChildAt(i) as CheckBox
+            if(view.isChecked) {
+                if(size_first) {
+                    querySize = "size=" + view.text.toString()
+                    size_first = false
+                }
+                else {
+                    querySize += "," + view.text.toString()
+                }
+            }
+        }
+    }
+
+    private fun determineQueryBrand() {
+        var brand_first = true
+        for(i in 0..(brand_container.childCount-1)) {
+            val view = brand_container.getChildAt(i) as CheckBox
+            if(view.isChecked) {
+                if(brand_first) {
+                    queryBrand = "brand=" + view.text.toString()
+                    brand_first = false
+                }
+                else {
+                    queryBrand += "," + view.text.toString()
+                }
+            }
+        }
+    }
+
+    private fun determineQueryColor() {
+        var color_first = true
+        for(i in 0..(color_container.childCount-1)) {
+            val view = color_container.getChildAt(i) as CheckBox
+            if(view.isChecked) {
+                if(color_first) {
+                    queryColor = "color=" + view.text.toString()
+                    color_first = false
+                }
+                else {
+                    queryColor += "," + view.text.toString()
+                }
+            }
+        }
+    }
+
+    private fun determineQueryRating() {
+        if(rating_radio_group.getCheckedRadioButtonId() != -1) {
+            val selectedId = rating_radio_group.getCheckedRadioButtonId()
+            val selectedRadioButton = resources.getResourceEntryName(selectedId)
+            Log.d("RADIO",selectedRadioButton)
+            val r = ratingFilterMap[selectedRadioButton.toString()]
+            Log.d("RADIO TO STR",selectedRadioButton.toString())
+            queryRating = "rating[gte]=" + r
+
+        }
+    }
+
+    private fun determineQueryMaxPrice() {
+        if(max_price.text.toString() != "") {
+            queryMaxPrice = "minPrice[lte]=" + max_price.text.toString()
+        }
+        else {
+            queryMaxPrice = ""
+        }
+    }
+
+    private fun determineQueryMinPrice() {
+        if(min_price.text.toString() != "") {
+            queryMinPrice = "minPrice[gte]=" + min_price.text.toString()
+        }
+        else {
+            queryMinPrice = ""
+        }
+    }
+
+    private fun initSearchView() {
         search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 if(query != "") {
@@ -300,14 +377,6 @@ class SearchFragment : Fragment() {
                 return false
             }
         })
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     private fun searchCall(limit: Int = 1000, page: Int = 1, fields: String = "", category: String = "", vendors: String = "") {
@@ -417,7 +486,7 @@ class SearchFragment : Fragment() {
     }
 
     // will be changed after milestone 2 :(
-    private fun placecat() {
+    private fun placeCategories() {
         val apiCallerGetCategories: ApiCaller<ResponseGetCategories> = ApiCaller(activity)
         //apiCallerLogin.Button = login_button
         apiCallerGetCategories.Caller = ApiClient.getClient.getCategories()
