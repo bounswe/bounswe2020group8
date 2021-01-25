33ed5bd50f6c3dd4a9f820858833a0585e62d654
OnurEnginer <onurenginer1@gmail.com>
2021-01-25 21:15:32 +0300

    Fixed comment rate and name display

--- Android/app/src/main/java/com/example/carousel/Comment.kt
+++ Android/app/src/main/java/com/example/carousel/Comment.kt
@@ -7,4 +7,7 @@ data class Comment(
     @Expose @SerializedName("mainProductId") val mainProductId: String,
     @Expose @SerializedName("customerId") val customerId: String,
     @Expose @SerializedName("text") val text: String,
-)
+    @Expose @SerializedName("rate") val rate: Int = 0,
+    @Expose @SerializedName("name") var name: String = "Anonymous",
+    @Expose @SerializedName("lastName") var lastName: String = "User",
+    )
