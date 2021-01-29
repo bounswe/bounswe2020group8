cb22194458d53d920040b3859042004a8855abf6
nursima çelik <nursima.celik@boun.edu.tr>
2021-01-29 22:37:11 +0300

    Add tests for price filter buttons

--- Android/app/src/androidTest/java/com/example/carousel/SearchFragmentTest.kt
+++ Android/app/src/androidTest/java/com/example/carousel/SearchFragmentTest.kt
@@ -1,16 +1,17 @@
 package com.example.carousel
 
-import android.app.Instrumentation
-import android.widget.ImageView
-import android.widget.RelativeLayout
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.SearchView
 import androidx.fragment.app.FragmentContainerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import org.junit.After
+import org.junit.Assert.assertNotNull
 import org.junit.Before
-
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
@@ -45,9 +46,65 @@ class SearchFragmentTest {
     }
 
     @Test
-    public fun testPriceFilter() {
+    public fun testButton_0_50() {
+        val button = mActivity?.findViewById<Button>(R.id.button_0_50)
+        button?.performClick()
 
+        val min_price = mActivity?.findViewById<EditText>(R.id.min_price)
+        assert(min_price?.text.toString() == "0")
+
+        val max_price = mActivity?.findViewById<EditText>(R.id.max_price)
+        assert(max_price?.text.toString() == "50")
     }
+
+    @Test
+    public fun testButton_50_100() {
+        val button = mActivity?.findViewById<Button>(R.id.button_50_100)
+        button?.performClick()
+
+        val min_price = mActivity?.findViewById<EditText>(R.id.min_price)
+        assert(min_price?.text.toString() == "50")
+
+        val max_price = mActivity?.findViewById<EditText>(R.id.max_price)
+        assert(max_price?.text.toString() == "100")
+    }
+
+    @Test
+    public fun testButton_100_250() {
+        val button = mActivity?.findViewById<Button>(R.id.button_100_250)
+        button?.performClick()
+
+        val min_price = mActivity?.findViewById<EditText>(R.id.min_price)
+        assert(min_price?.text.toString() == "100")
+
+        val max_price = mActivity?.findViewById<EditText>(R.id.max_price)
+        assert(max_price?.text.toString() == "250")
+    }
+
+    @Test
+    public fun testButton_250_500() {
+        val button = mActivity?.findViewById<Button>(R.id.button_250_500)
+        button?.performClick()
+
+        val min_price = mActivity?.findViewById<EditText>(R.id.min_price)
+        assert(min_price?.text.toString() == "250")
+
+        val max_price = mActivity?.findViewById<EditText>(R.id.max_price)
+        assert(max_price?.text.toString() == "500")
+    }
+
+    @Test
+    public fun testButton_500_plus() {
+        val button = mActivity?.findViewById<Button>(R.id.button_500_plus)
+        button?.performClick()
+
+        val min_price = mActivity?.findViewById<EditText>(R.id.min_price)
+        assert(min_price?.text.toString() == "500")
+
+        val max_price = mActivity?.findViewById<EditText>(R.id.max_price)
+        assert(max_price?.text.toString() == "")
+    }
+
 
     @After
     fun tearDown() {
